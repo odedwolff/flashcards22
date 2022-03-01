cdf94aa9736e66f63bc0ec500ddafbfcb46d7b0e
oded wolff <odedwolff@gmail.com>
2022-03-01 08:52:52 +0200

    added infrastructure for processing raw textfiles

--- app_root/proj_root/helpers/construct_data.js
+++ app_root/proj_root/helpers/construct_data.js
@@ -10,8 +10,19 @@ const RAW_DATA_FOLDER = '../raw_language_data/italian'
 
 exports.build_database = function(){
     fs.readdir(path.join(__dirname,RAW_DATA_FOLDER), (err, files) => {
-        files.forEach(file => {
-          console.log(file);
-        });
+        files.forEach(processFile);
       });
+}
+
+
+function processFile(filename){
+  const fullAame = 'proj_root/raw_language_data/italian/' + filename;
+  //const filePath = path.join(__dirname, filename);
+  fs.readFile(fullAame, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return;
+    }
+    console.log(data)
+  })
 }
